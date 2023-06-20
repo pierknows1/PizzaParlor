0531ecbaaee56a9d3a852d00f5f6a473f2f3c8fb
pier <pierknows@gmail.com>
2023-06-19 21:30:01 -0400

    added ui logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,7 @@
 //business logic
 
-function Pizza (size, toppings){
-    this.size = size;
-    this.toppings = toppings;
+function Pizza(small) {
+    this.size = small;
 }
 
 Pizza.protoytype.costSize = function() {
@@ -20,21 +19,61 @@ Pizza.protoytype.costSize = function() {
         costSize += 15;
     }
         return costSize;
-    };
+    }; 
 
-    Pizza.prototype.costToppings = function() {
-        let costToppings = 0;
-        if (this.costToppings === "Chicken"){
-            costToppings = 3;
-        }
-        else if (this.costToppings === "Pepperoni"){
-            costToppings = 4;
-        }
-        else if (this.costToppings === "sausage"){
-            costToppings = 5;
-        }
-        else if (this.costToppings === "Veggie"){
-            costToppings = 2;
-        }
-            return costToppings;
+
+Pizza.prototype.checkedToppings = function() {
+    let topping = [];
+    let toppingSelect = document.querySelectorAll("input[type=checkbox]:checked");
+    for (let i=0; i < toppingSelect.length; i++){
+        topping.push(toppingSelect[i].value);
     }
+    return topping;
+};
+
+function AddPizza (topping) {
+
+    this.topping = topping;
+}
+
+AddPizza.prototype.checkToppingsCost = function(){
+    let topping = [];
+    let toppingSelect = document.querySelectorAll("input[type=checkbox]:checked")
+    for (let i=0; i < toppingSelect.length; i++){
+        if (toppingSelect[i].checked){
+            topping.push(toppingSelect[i]);
+        }
+    }
+    return topping.length;
+};
+
+//ui logic
+
+function orderPizza(event){
+    event.preventDefault();
+    let pizzaSize = document.getAnimations("pizza-size").value;
+    let pizzaToppings = document.querySelectorAll("input[type=checkbox]:checked");
+    let newPizza = new Pizza(pizzaSize);
+    let newAddpizza = new AddPizza (pizzaToppings);
+    let toppings = newPizza.checkedToppings();
+    let totalOrder = document.getElementById("total-order");
+    let totalCost = document.getElementById("total-cost");
+    let pizzaTotal = newPizza.costSize() + newAddpizza.checkToppingsCost();
+    let totalSize = document.getElementById("size");
+    let checkedToppings = document.getElementById("toppings");
+    totalSize.innerText = newPizza.pizzaSize;
+    totalOrder.innerText = newAddpizza.pizzaToppings;
+    checkedToppings.innerText = toppings.join(", ");
+    totalCost.innerText = "$" + pizzaTotal;
+    totalOrder.removeAttribute("class");
+}
+
+    window.addEventListener("load", function(){
+        document.getElementById("order-pizza").addEventListener("submit,OrderPizza");
+    });
+
+
+
+
+
+
