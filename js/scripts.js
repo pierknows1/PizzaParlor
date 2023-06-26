33ade561a0cd76b05251897e35a52ca2f59eca7f
pier <pierknows@gmail.com>
2023-06-25 22:45:06 -0400

    fixed css and the js gives out the array when selecting the toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,45 @@
 //business logic
-
 function Pizza(size){
 
     this.size = size;
-
 }
 
 Pizza.prototype.crustCost = function () {
     let crustCost = 0.0;
     if (this.size === "Small") {
-      crustCost += 7.0;
+      crustCost += 5.0;
     } else if (this.size === "Medium") {
-      crustCost += 9.0;
+      crustCost += 8.0;
     } else if (this.size === "Large") {
-      crustCost += 20.0;
+      crustCost += 11.0;
     }
     return crustCost;
   };
+
+Pizza.prototype.toppings = function(){
+    let topping = [];
+    let toppingsList = document.querySelectorAll("input[type=checkbox]:checked");
+        for (let i = 0; i < toppingsList.length; i++){
+            topping.push(toppingsList[i].value);
+        }
+        return topping;
+};
+
+//this function adds the topping selection to the total
+function ToppingsPizza(){
+
+}
+
+ToppingsPizza.prototype.arrToppings = function(){
+  let topping = [];
+  let toppingsList = document.querySelectorAll("input[type=checkbox]:checked");
+        for (let i = 0; i < toppingsList.length; i++) {
+            if (toppingsList[i].checked) {
+                topping.push(toppingsList[i]);
+            }
+        }
+    return topping.length;
+};
 
 //ui logic
 
@@ -24,16 +47,21 @@ function OrderPizza (event){
     event.preventDefault();
 
     const crustSize = document.getElementById("pizza-size").value;
+    const toppingsCost = document.querySelectorAll("input[type=checkbox]:checked");
 
-    let newPizza = new Pizza(crustSize);
+    let newPizza = new Pizza(crustSize, toppingsCost);
+    let arrToppings = new ToppingsPizza();
+    let allToppings = newPizza.toppings();
     let showOrder = document.getElementById("show-order");
     let totalOrder = document.getElementById("total-order");
-    let totalCost = newPizza.crustCost();
+    let totalCost = newPizza.crustCost() + arrToppings.arrToppings();
     
     let pizzaSize = document.getElementById("size"); 
-
+    let toppings = document.getElementById("toppings");
+    
+    toppings.innerText = allToppings.join(", ");
     pizzaSize.innerText = newPizza.size;
-    totalOrder.innerText = "$" + totalCost;
+    totalOrder.innerText = " $" + totalCost;
     showOrder.removeAttribute("class");
 }
 
